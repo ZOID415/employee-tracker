08e62f5170a948d0152d265c0ad605dc6b43eacd
ZOID415 <michaelalfaro93@gmail.com>
2023-03-05 21:21:07 -0800

    Adds first prompt for inquirer

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const inquirer = require("inquirer");
+
+inquirer
+  .prompt([{
+    type: "list",
+    name: "prompt",
+    message: "What would you like to do?"
+    choices: ["View All Departments", "View All Roles", "View All Employees", "Add A Department", "Add An Employee", "Update An Employee Role", "Log Out"]
+}])
