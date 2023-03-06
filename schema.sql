5af8cf8a330ed2e890acbf9fb525093759385aa5
ZOID415 <michaelalfaro93@gmail.com>
2023-03-05 20:03:10 -0800

    adds seeds for department, roles, and employee insertion

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE department (
     name VARCHAR (30)
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT AUTO AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR (30),
     salary DECIMAL,
