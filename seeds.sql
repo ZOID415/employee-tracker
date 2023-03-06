1d066933718166be9505cf5ec4fa7a46c94d4b1d
ZOID415 <michaelalfaro93@gmail.com>
2023-03-05 20:13:21 -0800

    adds node modules schema.sql, and seeds.sql

--- seeds.sql
+++ seeds.sql
@@ -4,8 +4,17 @@ VALUES ("Engineering"),
        ("Finance"),
        ("Legal")
 
-INSERT INTO role (title, salary, department_id)
-VALUES (title),
-       (salary),
-       (department_id)
-       
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Software Engineer", 85000, 1),
+       ("Salesperson", 50000, 2),
+       ("Accountant", 120000, 4),
+       ("Lawyer", 200000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Allison", "Amareto", 1, 4),
+       ("Ben", "Babai", 2, 3),
+       ("Cameron", "Coats", 3, 2),
+       ("Damaris", "DeLeon", 4, 1);
+
+
+
