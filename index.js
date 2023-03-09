f649a41458243863883793187445181ab627c680
ZOID415 <michaelalfaro93@gmail.com>
2023-03-08 22:05:52 -0800

    adds index.js in db folder and adds code to index.js for inquirer prompt

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ inquirer
   .prompt([{
     type: "list",
     name: "prompt",
-    message: "What would you like to do?"
-    choices: ["View All Departments", "View All Roles", "View All Employees", "Add A Department", "Add An Employee", "Update An Employee Role", "Log Out"]
+    message: "What would you like to do?",
+    choices: [
+      {
+        name:"View All Department",
+        value:"VIEW_ALL_DEPARTMENT" },
+      { name:"View All Manager",
+        value:"VIEW_ALL_MANAGER" },
+      {name:"View All Employee",
+        value:"VIEW_ALL_EMPLOYEE" },
+//ADD EMPLOYEE, rtemove emplooyee, update, update empolyee manager
+//DO same thing for department and role
+      {name:"Add A Department",
+       value:"ADD_A_DEPARTMENT" }
+      {name:"Add An Employee", 
+    value:}
+      "Update An Employee Role", 
+      "Log Out"
+    ]
 }])
+//,then for choice
